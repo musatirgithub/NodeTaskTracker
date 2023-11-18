a8de38075c457dbf54b19042ca6799c70625691f
Mehmet Ugur Satir <musatir@gmail.com>
2023-11-19 00:06:55 +0100

    feat: Error classes construct

---
+++ errors/unauthorized.js
@@ -0,0 +1,11 @@
+const {CustomError} = require('.');
+const {StatusCodes} = require('http-status-codes')
+
+class UnauthorizedError extends CustomError{
+    constructor(message){
+        super(message);
+        this.statusCode = StatusCodes.FORBIDDEN;
+    }
+}
+
+module.exports = UnauthorizedError;
