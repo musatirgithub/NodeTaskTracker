0ac3a7fcd269bc3354c145ef145b4bd8d3dceb16
Mehmet Ugur Satir <musatir@gmail.com>
2023-11-19 23:57:19 +0100

    fix: htmlMessage in msg correction

--- utils/sendResetPasswordEmail.js
+++ utils/sendResetPasswordEmail.js
@@ -3,13 +3,13 @@ const sgMail = require('@sendgrid/mail');
 const sendResetPasswordEmail = async ({name, email, token, origin})=>{
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     const resetUrl = `${origin}/user/reset-password?token=${token}&email=${email}`;
-    const htmlMessage = `<p>Please reset password by clicking the following link : <a href="${resetURL}">Reset Password</a></p>`;
+    const htmlMessage = `<p>Please reset password by clicking the following link : <a href="${resetUrl}">Reset Password</a></p>`;
     const msg = {
         to:email,
         from:"musatir@gmail.com",
         subject:"Reset Password",
         html:`<h3>Hello ${name}</h3>
-        ${message}
+        ${htmlMessage}
         `,
     }
     const info = await sgMail.send(msg);
