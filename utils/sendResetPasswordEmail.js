b4591c8593fdd0c64ac237465e2a79e51edb0428
Mehmet Ugur Satir <musatir@gmail.com>
2023-11-19 23:48:15 +0100

    feat: sendResetPasswordEmail util establish

---
+++ utils/sendResetPasswordEmail.js
@@ -0,0 +1,18 @@
+const sgMail = require('@sendgrid/mail');
+
+const sendResetPasswordEmail = async ({name, email, token, origin})=>{
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const resetUrl = `${origin}/user/reset-password?token=${token}&email=${email}`;
+    const htmlMessage = `<p>Please reset password by clicking the following link : <a href="${resetURL}">Reset Password</a></p>`;
+    const msg = {
+        to:email,
+        from:"musatir@gmail.com",
+        subject:"Reset Password",
+        html:`<h3>Hello ${name}</h3>
+        ${message}
+        `,
+    }
+    const info = await sgMail.send(msg);
+}
+
+module.exports = sendResetPasswordEmail
