0845d34050bbf80dd4d6659ced74a164249c36a4
Mehmet Ugur Satir <musatir@gmail.com>
2023-11-19 21:23:03 +0100

    feat: checkPermissions util establish

---
+++ utils/checkPermissions.js
@@ -0,0 +1,9 @@
+const customError = require('../errors');
+
+const checkPermissions = (requestUser, resourceUserId)=>{
+    if(requestUser.role === 'admin') return;
+    if(requestUser.userId === resourceUserId.toString()) return;
+    throw new customError.CustomError.UnauthorizedError('Not authorized to access this route')
+}
+
+module.exports = checkPermissions;
