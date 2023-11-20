0c5a6efd8b4eb7e1dbe6d9d1a005e34f19e254df
Mehmet Ugur Satir <musatir@gmail.com>
2023-11-20 22:19:29 +0100

    fix: createHash require add

--- utils/index.js
+++ utils/index.js
@@ -3,5 +3,6 @@ const checkPermissions = require('./checkPermissions');
 const createTokenUser = require('./createTokenUser');
 const sendResetPasswordEmail = require('./sendResetPasswordEmail');
 const sendVerificationEmail = require('./sendVerificationEmail');
+const createHash = require('./createHash');
 
-module.exports = {createJWT, isTokenValid, attachCookiesToResponse, checkPermissions, createTokenUser, sendResetPasswordEmail, sendVerificationEmail}
+module.exports = {createJWT, isTokenValid, attachCookiesToResponse, checkPermissions, createTokenUser, sendResetPasswordEmail, sendVerificationEmail, createHash}
