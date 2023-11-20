b4574e0cfd7e9028a2b5ac8ea78c68a3a5208107
Mehmet Ugur Satir <musatir@gmail.com>
2023-11-20 22:18:57 +0100

    fix: CustomAPIError require fix

--- errors/unauthorized.js
+++ errors/unauthorized.js
@@ -1,7 +1,7 @@
-const {CustomError} = require('.');
+const CustomAPIError = require('./custom-api');
 const {StatusCodes} = require('http-status-codes')
 
-class UnauthorizedError extends CustomError{
+class UnauthorizedError extends CustomAPIError{
     constructor(message){
         super(message);
         this.statusCode = StatusCodes.FORBIDDEN;
