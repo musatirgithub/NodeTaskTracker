eeb79370c4d005502f88c15fa0aefaa83d71b163
Mehmet Ugur Satir <musatir@gmail.com>
2023-11-20 22:20:39 +0100

    fix: CustomAPIError require fix

--- errors/index.js
+++ errors/index.js
@@ -1,7 +1,7 @@
-const CustomError = require('./custom-api');
+const CustomAPIError = require('./custom-api');
 const BadRequest = require('./bad-request');
 const NotFound = require('./not-found');
 const UnauthenticatedError = require('./unauthenticated');
 const UnauthorizedError = require('./unauthorized');
 
-module.exports = {CustomError, BadRequest, NotFound, UnauthenticatedError, UnauthorizedError}
+module.exports = {CustomAPIError, BadRequest, NotFound, UnauthenticatedError, UnauthorizedError}
