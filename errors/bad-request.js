90794390fc17d1fa01ee13e819e3818727d87b83
Mehmet Ugur Satir <musatir@gmail.com>
2023-11-21 19:00:58 +0100

    fix: BadRequestError naming fix

--- errors/bad-request.js
+++ errors/bad-request.js
@@ -1,11 +1,11 @@
 const CustomAPIError = require('./custom-api');
 const {StatusCodes} = require('http-status-codes')
 
-class BadRequest extends CustomAPIError{
+class BadRequestError extends CustomAPIError{
     constructor(message){
         super(message);
         this.statusCode = StatusCodes.BAD_REQUEST;
     }
 }
 
-module.exports = BadRequest;
+module.exports = BadRequestError;
