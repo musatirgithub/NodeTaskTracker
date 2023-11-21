c3e3da221e11faff7420887a62800fa24f8f0376
Mehmet Ugur Satir <musatir@gmail.com>
2023-11-21 18:59:40 +0100

    fix: CustomError require fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,7 +11,7 @@ const register = async (req,res)=>{
 
     const emailAlreadyExists = await User.findOne({email});
     if(emailAlreadyExists){
-        throw new CustomError.BadRequest('Email already exists')
+        throw new CustomError.BadRequestError('Email already exists')
     };
 
     const isFirstAccount = (await User.countDocuments({})) === 0;
