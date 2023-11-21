a51ddd00f5e760a819f6ddeef71796a932e826a5
Mehmet Ugur Satir <musatir@gmail.com>
2023-11-21 19:01:37 +0100

    fix: Require naming fix

--- errors/index.js
+++ errors/index.js
@@ -1,7 +1,7 @@
 const CustomAPIError = require('./custom-api');
-const BadRequest = require('./bad-request');
-const NotFound = require('./not-found');
+const BadRequestError = require('./bad-request');
+const NotFoundError = require('./not-found');
 const UnauthenticatedError = require('./unauthenticated');
 const UnauthorizedError = require('./unauthorized');
 
-module.exports = {CustomAPIError, BadRequest, NotFound, UnauthenticatedError, UnauthorizedError}
+module.exports = {CustomAPIError, BadRequestError, NotFoundError, UnauthenticatedError, UnauthorizedError}
