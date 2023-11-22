f4f53cc6212cc0dc1636f9918085c8b5d608bc8b
Mehmet Ugur Satir <musatir@gmail.com>
2023-11-22 23:19:59 +0100

    feat: UpdateUserPassword controller establish

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,7 +40,19 @@ const updateUser = async (req,res)=>{
 }
 
 const updateUserPassword = async (req,res)=>{
-    res.send('update user password')
+    const {oldPassword, newPassword} = req.body;
+    if(!oldPassword || !newPassword){
+        throw new CustomError.BadRequestError('Please provide both old and new password!')
+    }
+    const user = await User.findOne({_id:req.user.userId});
+    const isPasswordCorrect = await user.comparePassword(oldPassword);
+    if(!isPasswordCorrect){
+        throw new CustomError.UnauthenticatedError('Invalid Credentials');
+    }
+    user.password = newPassword;
+    await user.save();
+
+    res.status(StatusCodes.OK).json({msg: 'Success! Password Updated.'})
 }
 const showMe = async (req,res)=>{
     res.status(StatusCodes).json({user:req.user});
