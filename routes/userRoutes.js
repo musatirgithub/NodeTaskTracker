101cd3f93889a9c687074c7ce96ada07f32c47bb
Mehmet Ugur Satir <musatir@gmail.com>
2023-11-22 22:31:23 +0100

    feat: Authentication & Permission establish

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const {authenticateUser, authorizePermissions} = require('../middlewares/authentication');
 const {getAllUsers, getSingleUser, updateUser, updateUserPassword, showMe} = require('../controllers/userController');
 
-router.get('/', getAllUsers);
-router.get('/showMe', showMe);
-router.patch('/update-user', updateUser);
-router.patch('/update-user-password', updateUserPassword);
-router.get('/:id', getSingleUser);
+router.get('/', authenticateUser, authorizePermissions('admin'), getAllUsers);
+router.get('/showMe', authenticateUser, showMe);
+router.patch('/update-user', authenticateUser, updateUser);
+router.patch('/update-user-password', authenticateUser, updateUserPassword);
+router.get('/:id', authenticateUser, getSingleUser);
 
 module.exports = router;
