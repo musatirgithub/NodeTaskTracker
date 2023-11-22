fe5d34fc74549e60ff775a6d3da9fa8981d2aab9
Mehmet Ugur Satir <musatir@gmail.com>
2023-11-22 22:32:00 +0100

    feat: GetAllUsers controller establish

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,13 @@
 
+const User = require('../models/user');
+const {StatusCodes} = require('http-status-codes');
+const {checkPermissions} = require('../utils');
 
 const getAllUsers = async (req,res)=>{
-    res.send('get all users')
+    
+    const users = await User.find({role:'user'}).select('-password');
+
+    res.status(StatusCodes.OK).json({users})
 }
 
 const getSingleUser = async (req,res)=>{
@@ -14,6 +20,7 @@ const updateUserPassword = async (req,res)=>{
     res.send('update user password')
 }
 const showMe = async (req,res)=>{
+
     res.send('show current user')
 }
 
