e142eece222904aef117efd19ae3abc1278ee486
Mehmet Ugur Satir <musatir@gmail.com>
2023-11-23 22:26:50 +0100

    feat: TaskControllers establish

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,22 +1,47 @@
+const Task = require('../models/task');
+const CustomError = require('../errors')
+const {StatusCodes} = require('http-status-codes');
 
 
 const getAllTasks = async (req, res)=>{
-    req.send('get all tasks')
+    const tasks = await Task.find({}).sort('-isDone deadline')
+    res.send('get all tasks')
 }
 const getTasks = async (req, res)=>{
-    req.send('get users tasks')
+    const tasks = await Task.find({user:req.user.userId}).select('-user').sort('-isDone deadline')
+    res.status(StatusCodes.OK).json({tasks})
 }
 const createTask = async (req, res)=>{
-    req.send('create task')
+    const {name, deadline} = req.body;
+    if(!name || !deadline){
+        throw new CustomError.BadRequestError('Please provide both name and deadline')
+    }
+    const task = await Task.create({name, deadline, user:req.user.userId})
+    res.status(StatusCodes.CREATED).json({msg:'Success! Task created'})
 }
 const deleteTasks = async (req, res)=>{
-    req.send('delete task')
+    const {id:taskId} = req.params;
+    const task = await Task.findOneAndDelete({_id:taskId});
+    if(!task){
+        throw new CustomError.NotFoundError(`No task with id: ${taskId}`)
+    }
+    res.status(StatusCodes.OK).json({msg:'Task deleted successfully'})
 }
 const updateTask = async (req, res)=>{
-    req.send('update task')
+    const {id:taskId} = req.params;
+    const task = await Task.findOneAndUpdate({_id:taskId}, req.body, {new:true, runValidators:true}).select('-user')
+    if(!task){
+        throw new CustomError.NotFoundError(`No task with ID: ${taskId}`);
+    }
+    res.status(StatusCodes.OK).json({task})
 }
 const getTask = async (req, res)=>{
-    req.send('get single task')
+    const {id:taskId} = req.params;
+    const task = await Task.findOne({_id:taskId}).select('-user');
+    if(!task){
+        throw new CustomError.NotFoundError(`No task with ID: ${taskId}`);
+    }
+    res.status(StatusCodes.OK).json({task})
 }
 
 module.exports = {getAllTasks, getTasks, createTask, deleteTasks, updateTask, getTask};
