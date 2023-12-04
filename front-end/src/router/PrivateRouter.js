3cc4c9abfc6368fdc0f5f42be2160e5dffacb7e1
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-04 23:28:48 +0100

    feat: PrivateRouter setup

---
+++ front-end/src/router/PrivateRouter.js
@@ -0,0 +1,7 @@
+import {useSelector} from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+
+const PrivateRouter = ()=>{
+    const {currentUser} = useSelector((state)=>state.auth);
+    return currentUser ? <Outlet /> : <Navigate to='/login' />
+}
