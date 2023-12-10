4309347a5f6fefbd893426415e793905e517c2f4
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-10 16:23:28 +0100

    feat: ForgotPassword page add

--- server/public/src/router/AppRouter.js
+++ server/public/src/router/AppRouter.js
@@ -10,6 +10,7 @@ import Tasks from "../pages/Tasks";
 import Home from "../pages/Home";
 import VerifyEmail from "../pages/VerifyEmail";
 import VerifyEmailWarning from "../pages/VerifyEmailWarning";
+import ForgotPassword from "../pages/ForgotPassword";
 
 import Navbar from "../components/Navbar";
 
@@ -22,6 +23,7 @@ const AppRouter = ()=>{
                 <Route path="/register" element={<Register/>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/reset-password" element={<ResetPassword/>}/>
+                <Route path="/forgot-password" element={<ForgotPassword/>}/>
                 <Route path="/user/verify-email" element={<VerifyEmail/>}/>
                 <Route path="/verify-email-warning" element={<VerifyEmailWarning/>}/>
                 <Route path="/tasks" element={<PrivateRouter/>}>
