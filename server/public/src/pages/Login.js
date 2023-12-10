84f878023302bbdb700d87ab674209feba9650e0
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-10 22:24:21 +0100

    style: Login form style

--- server/public/src/pages/Login.js
+++ server/public/src/pages/Login.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import useAuthCalls from "../hooks/useAuthCalls";
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
@@ -16,12 +17,21 @@ const Login = () => {
     }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center min-h-[screen-10rem]">
+      <section>
+        <div className="flex gap-3">
       <label htmlFor="email">Email</label>
       <input type="email" name="email" id="email" required value={userInfo.email} onChange={handleChange}/>
+      </div>
+      <div className="flex gap-3">
       <label htmlFor="password">Password</label>
       <input type="password" name="password" id="password" required value={userInfo.password} onChange={handleChange}/>
-      <button type="submit" className="text-2xl underline">Submit</button>
+      </div>
+      </section>
+      <h4>
+        <Link to='/forgot-password' className="btn btn-link">Forgot Password?</Link>
+      </h4>
+      <button type="submit" className="btn btn-neutral">Submit</button>
     </form>
   )
 }
