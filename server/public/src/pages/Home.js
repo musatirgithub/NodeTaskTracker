e71d1623a819b15bad577a27310086aaac7a845a
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-14 00:12:25 +0100

    feat: Home page basic setup

--- server/public/src/pages/Home.js
+++ server/public/src/pages/Home.js
@@ -1,6 +1,26 @@
+import {useEffect, useState} from "react";
+import { useSelector } from "react-redux";
+import TaskForm from "../components/TaskForm";
+import TaskList from "../components/TaskList";
+import useTaskCalls from "../hooks/useTaskCalls";
+
 const Home = () => {
+  const [loading, setLoading] = useState(true)
+  const {getTasks} = useTaskCalls();
+  const {tasks} = useSelector((state)=>state.task);
+
+  useEffect(() => {
+    setLoading(true);
+    getTasks();
+    setLoading(false);
+  }, [])
+  
+
   return (
-    <div>Home</div>
+    <div>
+      <TaskForm/>
+      {/* <TaskList tasks={tasks}/> */}
+    </div>
   )
 }
 
