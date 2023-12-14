a4ecad4df9e786be23c4befa70da205b0c86e2bc
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-14 21:28:58 +0100

    feat: CurrentUser add

--- server/public/src/components/Navbar.js
+++ server/public/src/components/Navbar.js
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
 import useAuthCalls from "../hooks/useAuthCalls";
+import { useSelector } from "react-redux";
 
 
 const Navbar = () => {
+  const {currentUser, role} = useSelector((state)=>state.auth)
   const {logout} = useAuthCalls();
   return (
     <nav className="flex justify-between px-5">
+      <div className="flex gap-3">
       <h4>Ich bin Logo</h4>
+      <h4>{currentUser?`Welcome ${currentUser}`:'Welcome visitor'}</h4>
+      </div>
       <div className="flex justify-evenly gap-3">
       <Link to='/register'>Register</Link>
       <Link to='/login'>Login</Link>
