6c786fa810437c37bf6ba9b16ae0cd349d671b0f
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-15 23:51:43 +0100

    feat: UpdateTaskModal component add

--- server/public/src/pages/Home.js
+++ server/public/src/pages/Home.js
@@ -1,29 +1,26 @@
-import {useEffect, useState} from "react";
+import { useState, useEffect } from "react";
 import TaskForm from "../components/TaskForm";
 import TaskList from "../components/TaskList";
-import useTaskCalls from "../hooks/useTaskCalls";
+import { useSelector } from "react-redux";
+import UpdateTaskModal from "../components/UpdateTaskModal";
 
 const Home = () => {
-  // const [loading, setLoading] = useState(true)
-  // const {getTasks} = useTaskCalls();
+  const {isModalOpen} = useSelector((state)=>state.task);
+const [loading, setLoading] = useState(true)
 
+useEffect(() => {
+  setLoading(false)
+}, [])
 
-  // useEffect(() => {
-  //   setLoading(true);
-  //   getTasks();
-  //   setLoading(false);
-  // }, [])
-  
-  // if(loading){
-  //   return(
-  //     <div>Loading...</div>
-  //   )
-  // }
+if(loading){
+  return <div>Loading...</div>
+}
 
   return (
-    <div>
+    <div className="relative">
       <TaskForm/>
       <TaskList/>
+      {isModalOpen && <UpdateTaskModal/>}
     </div>
   )
 }
