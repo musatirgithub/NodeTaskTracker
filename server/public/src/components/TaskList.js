f90fec73f027a084f6ec6fd8644bcfffcf124afd
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-15 23:48:11 +0100

    feat: Update delete icons & functionality add

--- server/public/src/components/TaskList.js
+++ server/public/src/components/TaskList.js
@@ -1,14 +1,20 @@
 import { useSelector } from "react-redux";
 import {useEffect, useState} from "react";
 import useTaskCalls from "../hooks/useTaskCalls";
+import { FaPencilAlt } from "react-icons/fa";
+import { FaRegTrashCan } from "react-icons/fa6";
+import UpdateTaskModal from "./UpdateTaskModal";
+import { useDispatch } from "react-redux";
+import { openModal } from "../features/taskSlice";
 
 const TaskList = () => {
+    const dispatch = useDispatch();
     const {tasks} = useSelector((state)=>state.task);
     
 
     const [loading, setLoading] = useState(true)
-    const {getTasks} = useTaskCalls();
-  
+    const {getTasks, deleteTask, getTask} = useTaskCalls();
+    
   
     useEffect(() => {
       setLoading(true);
@@ -16,18 +22,24 @@ const TaskList = () => {
       setLoading(false);
     }, [])
 
+    const handleClick = (id)=>{
+        getTask(id);
+        dispatch(openModal());
+    }
     if(loading){
         return(
             <div>Loading...</div>
         )
     }
   return (
-    <div>
+    <div className="p-5">
         {tasks?.map((task, index)=>{
             return(
-                <div key={index}>
+                <div key={index} className="flex gap-5 justify-center items-center relative">
                     <h4>{task.name}</h4>
                     <h4>{task.deadline}</h4>
+                    <FaPencilAlt onClick={()=>handleClick(task._id)} className="cursor-pointer"/>
+                    <FaRegTrashCan onClick={()=>deleteTask(task._id)} className="cursor-pointer"/>
                 </div>
             )
         })}
