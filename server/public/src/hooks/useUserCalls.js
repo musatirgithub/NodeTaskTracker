6411d83be7db08d20ed4cc38ddb70e79b8c3458f
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-17 20:23:49 +0100

    feat: useUserCalls setup

---
+++ server/public/src/hooks/useUserCalls.js
@@ -0,0 +1,42 @@
+import { useDispatch } from "react-redux";
+import {
+  fetchStart,
+  updatePasswordSuccess,
+  fetchFail,
+} from "../features/userSlice";
+import {axiosPublic} from "../utils/axiosPublic";
+// import { toastSuccessNotify, toastErrorNotify } from "../helper/ToastNotify";
+
+const useTaskCalls = () => {
+  const dispatch = useDispatch();
+
+  // const getRecordAndMediator = async () => {
+  //   dispatch(fetchStart());
+  //   try {
+  //     const [record, mediator] = await Promise.all([
+  //       axiosWithToken.get("api/record/"),
+  //       axiosWithToken.get("api/mediator/"),
+  //     ]);
+
+  //     dispatch(getRecordAndMediatorSuccess([record?.data, mediator?.data]));
+  //   } catch (error) {
+  //     dispatch(fetchFail());
+  //   }
+  // };
+
+  const updatePassword = async (userInfo) => {
+    dispatch(fetchStart());
+    try {
+      await axiosPublic.patch("/api/v1/user/update-user-password", userInfo, {withCredentials:'include'});
+      dispatch(updatePasswordSuccess());
+    } catch (err) {
+      dispatch(fetchFail());
+    }
+  };
+ 
+  return {
+    updatePassword,
+  };
+};
+
+export default useTaskCalls;
