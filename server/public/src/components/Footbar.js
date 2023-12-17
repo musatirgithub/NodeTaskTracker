78fdc9af1bd5858b5b445aeed1d03c62bbdbfe78
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-17 20:21:48 +0100

    feat: Footbar add

---
+++ server/public/src/components/Footbar.js
@@ -0,0 +1,10 @@
+
+const Footbar = () => {
+  return (
+    <section className="flex justify-center items-center bg-[#D9C6A7] h-[4rem]">
+        <div className="text-[#0D1732] text-[1.2rem] font-semibold">© {new Date().getFullYear()} Shutter Software</div>
+    </section>
+  )
+}
+
+export default Footbar
