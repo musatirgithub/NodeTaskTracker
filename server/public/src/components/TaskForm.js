5e3d8bb360249d8d150008330800790421b13291
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-18 23:01:35 +0100

    style: TaskForm style

--- server/public/src/components/TaskForm.js
+++ server/public/src/components/TaskForm.js
@@ -17,22 +17,23 @@ const TaskForm = () => {
     }
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center min-h-[screen-10rem]">
-      <section>
-        <div className="flex gap-3">
-      <label htmlFor="name">Task</label>
-      <input type="text" name="name" id="name" required value={formData.task} onChange={handleChange}/>
+    <section className="p-5 bg-slate-800 mx-auto lg:max-w-5xl">
+    <form onSubmit={handleSubmit} className="flex justify-center items-center gap-3" autoComplete="off">
+      <section className="flex gap-3">
+        <div className="flex flex-col gap-3">
+      <label htmlFor="name" className="text-[#EEEDE8]">Task</label>
+      <input type="text" name="name" id="name" required value={formData.task} onChange={handleChange}
+      className="bg-[#D9C6A7] text-[#0D1732] focus:outline-none border border-gray-300 rounded py-2 px-4 block appearance-none placeholder-[#3A3B3C] h-[2.5rem] lg:w-[30rem]"/>
       </div>
-      <div className="flex gap-3">
-      <label htmlFor="deadline">Deadline</label>
-      <input type="datetime-local" name="deadline" id="deadline" required value={formData.deadline} onChange={handleChange}/>
+      <div className="flex  flex-col gap-3">
+      <label htmlFor="deadline" className="text-[#EEEDE8]">Deadline</label>
+      <input type="datetime-local" name="deadline" id="deadline" required value={formData.deadline} onChange={handleChange}
+      className="bg-[#D9C6A7] text-[#0D1732] focus:outline-none border border-gray-300 rounded py-2 px-4 block appearance-none placeholder-[#3A3B3C] h-[2.5rem] w-[9.5rem]"/>
       </div>
       </section>
-      <h4>
-        <Link to='/forgot-password' className="btn btn-link">Forgot Password?</Link>
-      </h4>
       <button type="submit" className="btn btn-neutral">Submit</button>
     </form>
+    </section>
   )
 }
 
