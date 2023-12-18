201421fe54049008f3150046444248b5308dd0de
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-18 22:59:22 +0100

    style: Logo update

--- server/public/src/components/Navbar.js
+++ server/public/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import useAuthCalls from "../hooks/useAuthCalls";
 import { useSelector } from "react-redux";
+import logo from "../pics/logo.jpg";
 
 
 const Navbar = () => {
@@ -8,14 +9,18 @@ const Navbar = () => {
   const {logout} = useAuthCalls();
   return (
     <nav className="flex justify-between items-center h-[4rem] px-5  bg-[#D9C6A7] text-[#0D1732] text-[1rem] font-semibold">
-      <div className="flex gap-3">
-      <h4>Ich bin Logo</h4>
+      <div className="flex items-center gap-5">
+      <div>
+        <Link to='/'>
+        <img src={logo} className="w-[2.5rem]"/>
+        </Link>
+      </div>
       <h4>{currentUser?`Welcome ${currentUser}`:'Welcome visitor'}</h4>
       </div>
       <div className="flex justify-evenly gap-3">
       <Link to='/register'>Register</Link>
       <Link to='/login'>Login</Link>
-      <h4 onClick={logout}>Logout</h4>
+      <h4 onClick={logout} className="cursor-pointer">Logout</h4>
       </div>
 
     </nav>
