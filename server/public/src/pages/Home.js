6420bb32b0607c5dff194deba0cb41e2fc0a330b
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-18 23:04:18 +0100

    style: Home page style

--- server/public/src/pages/Home.js
+++ server/public/src/pages/Home.js
@@ -17,7 +17,7 @@ if(loading){
 }
 
   return (
-    <div className="relative">
+    <div className="relative min-h-[calc(100vh-8rem)] bg-[#0D1732]">
       <TaskForm/>
       <TaskList/>
       {isModalOpen && <UpdateTaskModal/>}
