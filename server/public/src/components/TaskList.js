58a845f2709cb18501c98aa139e565f74f740a16
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-20 23:02:44 +0100

    style: Conditional style

--- server/public/src/components/TaskList.js
+++ server/public/src/components/TaskList.js
@@ -3,9 +3,12 @@ import {useEffect, useState} from "react";
 import useTaskCalls from "../hooks/useTaskCalls";
 import { FaPencilAlt } from "react-icons/fa";
 import { FaRegTrashCan } from "react-icons/fa6";
+import { IoWarning } from "react-icons/io5";
 import UpdateTaskModal from "./UpdateTaskModal";
 import { useDispatch } from "react-redux";
 import { openModal } from "../features/taskSlice";
+import dateTimeConverter from "../utils/dateTimeConverter";
+import remainingDays from "../utils/remainingDays";
 
 const TaskList = () => {
     const dispatch = useDispatch();
@@ -34,14 +37,24 @@ const TaskList = () => {
         return <section className="text-center pt-5 text-[#EEEDE8]">No tasks to show...</section>
     }
   return (
-    <div className="p-5 text-[#EEEDE8]">
+    <div className="p-1 text-[#EEEDE8]">
+        <h3 className="text-center text-2xl">Tasks</h3>
+        <div className="flex lg:w-[64rem] mx-auto">
+            <h4 className="lg:w-[35rem] text-center">Task</h4>
+            <h4 className="lg:w-[19rem] text-center">Deadline</h4>
+            <h4 className="lg:w-[5rem] text-center">Edit</h4>
+            <h4 className="lg:w-[5rem] text-center">Delete</h4>
+        </div>
         {tasks?.map((task, index)=>{
+            const remainder = remainingDays(task.deadline)
+            console.log("remainder", index, remainder)
             return(
-                <div key={index} className="flex gap-5 justify-center items-center relative">
-                    <h4>{task.name}</h4>
-                    <h4>{task.deadline}</h4>
-                    <FaPencilAlt onClick={()=>handleClick(task._id)} className="cursor-pointer"/>
-                    <FaRegTrashCan onClick={()=>deleteTask(task._id)} className="cursor-pointer"/>
+                <div key={index} className="flex gap-5 justify-center items-center lg:w-[64rem] mx-auto">
+                    {remainder && <IoWarning color={remainder}/>}
+                    <h4 className="lg:w-[35rem] text-center">{task.name}</h4>
+                    <h4 className="lg:w-[19rem] text-center">{dateTimeConverter(new Date(task.deadline))}</h4>
+                    <h4 className="lg:w-[5rem] text-center"><FaPencilAlt onClick={()=>handleClick(task._id)} className="cursor-pointer"/></h4>
+                    <h4 className="lg:w-[5rem] text-center"><FaRegTrashCan onClick={()=>deleteTask(task._id)} className="cursor-pointer"/></h4>
                 </div>
             )
         })}
