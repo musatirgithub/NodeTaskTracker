4689296e654e923ac0ec39b2cc478b00f4712531
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-20 22:54:02 +0100

    feat: RemainingDays func add

---
+++ server/public/src/utils/remainingDays.js
@@ -0,0 +1,24 @@
+
+const remainingDays = (deadline) => {
+    const currentDayTime = new Date(Date.now());
+    const diff = ((new Date(deadline)) - currentDayTime)/ (1000 * 60 * 60)
+    console.log("currentDayTime", currentDayTime);
+    console.log("deadline", new Date(deadline));
+    console.log("diff", diff)
+    if(diff > 48){
+        return
+    }
+    let color = ''
+    if(diff<= 48){
+        color = 'yellow';
+    }
+    if(diff<= 24){
+        color = 'orange';
+    }
+    if(diff< 0){
+        color = 'red';
+    }
+    return color
+}
+
+export default remainingDays
