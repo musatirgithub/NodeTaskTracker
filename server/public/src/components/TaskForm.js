95b5e4e62da66baf73c285f009ba1c19f9065718
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-20 22:06:47 +0100

    style: TasForm style change

--- server/public/src/components/TaskForm.js
+++ server/public/src/components/TaskForm.js
@@ -17,21 +17,21 @@ const TaskForm = () => {
     }
 
   return (
-    <section className="p-5 bg-slate-800 mx-auto lg:max-w-5xl">
-    <form onSubmit={handleSubmit} className="flex justify-center items-center gap-3" autoComplete="off">
+    <section className="p-5 mx-auto lg:max-w-5xl ">
+    <form onSubmit={handleSubmit} className="flex justify-center items-center gap-3 border-2 rounded-3xl mt-3 pb-3" autoComplete="off">
       <section className="flex gap-3">
         <div className="flex flex-col gap-3">
       <label htmlFor="name" className="text-[#EEEDE8]">Task</label>
-      <input type="text" name="name" id="name" required value={formData.task} onChange={handleChange}
+      <input type="text" name="name" id="name" required value={formData.name} onChange={handleChange}
       className="bg-[#D9C6A7] text-[#0D1732] focus:outline-none border border-gray-300 rounded py-2 px-4 block appearance-none placeholder-[#3A3B3C] h-[2.5rem] lg:w-[30rem]"/>
       </div>
       <div className="flex  flex-col gap-3">
       <label htmlFor="deadline" className="text-[#EEEDE8]">Deadline</label>
       <input type="datetime-local" name="deadline" id="deadline" required value={formData.deadline} onChange={handleChange}
-      className="bg-[#D9C6A7] text-[#0D1732] focus:outline-none border border-gray-300 rounded py-2 px-4 block appearance-none placeholder-[#3A3B3C] h-[2.5rem] w-[9.5rem]"/>
+      className="bg-[#D9C6A7] text-[#0D1732] focus:outline-none border border-gray-300 rounded py-2 px-4 block appearance-none placeholder-[#3A3B3C] h-[2.5rem] w-[12rem]"/>
       </div>
       </section>
-      <button type="submit" className="btn btn-neutral">Submit</button>
+      <button type="submit" className="btn btn-danger mt-9">Add Task</button>
     </form>
     </section>
   )
