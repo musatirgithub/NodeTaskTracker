e0d8b245d6b3cac607e1aedeeb4e71db2f999da4
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-23 23:04:19 +0100

    style: ResetPassword page style

--- server/public/src/pages/ResetPassword.js
+++ server/public/src/pages/ResetPassword.js
@@ -33,15 +33,25 @@ const ResetPassword = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-5 min-h-[calc(100vh-10rem)]">
-    <section>
-    <div className="flex gap-3">
-    <label htmlFor="password">Password</label>
-    <input type="password" name="password" id="password" required value={password} onChange={handleChange}/>
-    </div>
-    </section>
-    <button type="submit" className="btn btn-neutral">Submit New Password</button>
-  </form>
+    <main className="flex justify-evenly items-center bg-[#0D1732] min-h-[calc(100vh-8rem)]">
+      <div className="hidden lg:block w-[45rem] ">
+        <img src = "https://images.pexels.com/photos/17155842/pexels-photo-17155842/free-photo-of-finger-scan.jpeg?auto=compress&cs=tinysrgb&w=600"
+       alt="login"
+       className="object-cover"/>
+      </div>
+      
+
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3 justify-center items-center min-h-[screen-10rem]" autoComplete="off">
+      <section className="flex flex-col gap-3 mb-5">
+      <div className="flex  flex-col gap-3">
+      <label htmlFor="password" className="text-[#EEEDE8]">New Password</label>
+      <input type="password" name="password" id="password" required value={password} onChange={handleChange} placeholder="Enter new password..."
+      className="bg-[#D9C6A7] text-[#0D1732] focus:outline-none border border-gray-300 rounded py-2 px-4 block w-full appearance-none placeholder-[#3A3B3C]"/>
+      </div>
+      </section>
+      <button type="submit" className={`btn bg-[#F8B664] hover:bg-[#C6A779] text-[#0D1732] w-full`}>Submit New Password</button>
+    </form>
+    </main>
   )
 }
 
