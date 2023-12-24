7c2764ab164b271af69cf567e4248af2abb25984
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-24 19:01:50 +0100

    feat: Task completed input and state add

--- server/public/src/components/UpdateTaskModal.js
+++ server/public/src/components/UpdateTaskModal.js
@@ -9,17 +9,16 @@ const UpdateTaskModal = () => {
   const dispatch = useDispatch();
     const {updateTask} = useTaskCalls();
     const {task} = useSelector((state)=>state.task);
-    const [formData, setFormData] = useState({name:task?.name, deadline:task?.deadline});
+    const [checkData, setCheckData] = useState(task?.completed)
+    const [formData, setFormData] = useState({name:task?.name, deadline:task?.deadline, completed:checkData});
 
-    console.log("task", task);
     const handleChange = (e)=>{
         setFormData({...formData, [e.target.name]:e.target.value})
     }
 
     const handleSubmit = (e)=>{
         e.preventDefault();
-        updateTask(formData, task._id);
-        setFormData({name:'', deadline:''});
+        updateTask({name:formData.name, deadline:formData.deadline, completed:checkData}, task._id);
         dispatch(closeModal());
     }
 
@@ -29,19 +28,28 @@ const UpdateTaskModal = () => {
     
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center min-h-[screen-10rem]">
-      <section>
+    <form onSubmit={handleSubmit} className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-10  bg-[#D9C6A7] rounded-xl p-3 w-[21rem]">
+      <div className="text-end text-[#2B3440]  text-[1.2rem] font-extrabold cursor-pointer" onClick={()=>dispatch(closeModal())}>X</div>
+      <div className="flex flex-col justify-center items-center gap-3">
+      <section className="flex flex-col gap-3">
         <div className="flex gap-3">
-      <label htmlFor="name">Task</label>
-      <input type="text" name="name" id="name" required value={formData.name} onChange={handleChange}/>
+      {/* <label htmlFor="name">Task</label> */}
+      <input type="text" name="name" id="name" required value={formData.name} onChange={handleChange}
+      className="w-[15rem] h-[2rem] rounded ps-2"/>
       </div>
       <div className="flex gap-3">
-      <label htmlFor="deadline">Deadline</label>
-      <input type="datetime-local" name="deadline" id="deadline" required value={formData.deadline} onChange={handleChange}/>
+      {/* <label htmlFor="deadline">Deadline</label> */}
+      <input type="datetime-local" name="deadline" id="deadline" required value={formData.deadline} onChange={handleChange}
+      className="w-[15rem] h-[2rem] rounded ps-2"/>
+      </div>
+      <div className="flex justify-center gap-3">
+      <input type="checkbox" name="completed" checked={checkData} onChange={()=>setCheckData(!checkData)}></input>
+      <label htmlFor="completed">Task completed?</label>
       </div>
       </section>
 
       <button type="submit" className="btn btn-neutral">Update Task</button>
+      </div>
     </form>
   )
 }
