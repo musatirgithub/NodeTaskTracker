95890d0f0daf86ccb2110c71a26f926d73ea5e14
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-25 21:13:18 +0100

    fix: Unnecessary lines removed

--- server/public/src/features/authSlice.js
+++ server/public/src/features/authSlice.js
@@ -26,12 +26,6 @@ const authSlice = createSlice({
       state.currentUser = null;
       state.role = null;
     },
-    // registrationSuccess: (state, { payload }) => {
-    //   state.loading = false;
-    //   state.error = false;
-    //   state.currentUser = payload?.user?.name;
-    //   state.role = payload?.user?.role;
-    // },
     validationSuccess: (state) => {
       state.loading = false;
       state.error = false;
@@ -51,7 +45,6 @@ export const {
   fetchStart,
   loginSuccess,
   logoutSuccess,
-  // registrationSuccess,
   validationSuccess,
   fetchFail,
   tokenTimeout,
