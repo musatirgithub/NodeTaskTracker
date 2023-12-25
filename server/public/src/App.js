c4224a494c7c0f8867436894aa9eb41310e02402
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-25 21:03:43 +0100

    feat: React Toastify App Wrap

--- server/public/src/App.js
+++ server/public/src/App.js
@@ -2,15 +2,19 @@ import { Provider } from 'react-redux';
 import './App.css';
 import AppRouter from "./router/AppRouter"
 import { PersistGate } from 'redux-persist/integration/react';
+import { ToastContainer } from "react-toastify";
 import store, { persistor } from './app/store';
 
 function App() {
   return (
+    <>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <AppRouter />
       </PersistGate>
     </Provider>
+    <ToastContainer />
+    </>
   );
 }
 
