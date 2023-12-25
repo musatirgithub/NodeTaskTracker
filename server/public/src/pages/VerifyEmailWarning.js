63289cdbe393c684d63482c41969a1ff04ebdc75
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-25 21:04:46 +0100

    style: VerifyEmailWarning style

--- server/public/src/pages/VerifyEmailWarning.js
+++ server/public/src/pages/VerifyEmailWarning.js
@@ -1,13 +1,14 @@
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
 
 const VerifyEmailWarning = () => {
   return (
-    <main className="max-w-xl mx-auto p-5 min-h-vh">
+    <main className="max-w-xl mx-auto p-5 min-h-[calc(100vh-8rem)] flex flex-col justify-center items-center gap-5">
         <h3 className="text-xl ">A confirmation link has been sent to your E-mail Address.</h3>
-        <h5 className="text-md bg-red-700 text-white">Please don't forget to check spam mails if you don't get the mail!</h5>
-        <Link to='/login'>
+        <h3 className="text-xl ">After confirming your E-mail Address please login.</h3>
+        <h5 className="text-lg text-red-700 font-bold">Please check spam, in case you don't find the mail in inbox!</h5>
+        {/* <Link to='/login'>
         <button className="btn btn-accent">Login Page</button>
-        </Link>
+        </Link> */}
     </main>
   )
 }
