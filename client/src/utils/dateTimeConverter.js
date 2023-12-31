5a40ccecf7db81baf563be9813518dbee99ad649
Mehmet Ugur Satir <musatir@gmail.com>
2023-12-31 21:11:01 +0100

    fix: month day presentation fox

--- client/src/utils/dateTimeConverter.js
+++ client/src/utils/dateTimeConverter.js
@@ -1,8 +1,14 @@
 
 const dateTimeConverter = (dateTime)=>{
     // const year = dateTime.getFullYear();
-    const month = dateTime.getMonth();
-    const date = dateTime.getDate();
+    const month = (dateTime.getMonth() +1).toString();
+    const date = dateTime.getDate().toString();
+    if (month.length === 1){
+        month = "0" + month;
+    }
+    if (date.length === 1){
+        date = "0" + date;
+    }
     // const hour = dateTime.getHours();
     // const minute = dateTime.getMinutes();
     return `${date}.${month}`
